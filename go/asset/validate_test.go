75a638b5fa06b96f9ba5e3ee6bae3c74ae5c21ef
Asanka Herath <asanka@chromium.org>
2018-04-16 11:56:37 -0400

    Add a test to assert that a WindowsMachine is-a Machine.
    The ProtoBuf schema doesn't have a way to intrinsically assert this. So
    we have to do it manually.
    
    Change-Id: I615e2984f4426f9c0fe0a6410e07e72798ea3b10
    Reviewed-on: https://chromium-review.googlesource.com/1000438
    Reviewed-by: Fei Ling <feiling@chromium.org>

--- go/asset/validate_test.go
+++ go/asset/validate_test.go
@@ -22,3 +22,44 @@ func TestAssetManifest_validateFields(t *testing.T) {
 		t.Fatal("unexpected error ", err)
 	}
 }
+
+// Tests that all methods in Machine are present in WindowsMachine.
+//
+// Note: If any new machine types are added to the schema, they should also
+// have a <new-machine-type>IsMachine test like this one.
+func TestActiveDirectory_windowsMachineIsMachine(t *testing.T) {
+	mt := reflect.TypeOf(&Machine{})
+	wmt := reflect.TypeOf(&WindowsMachine{})
+
+	for i := 0; i < mt.NumMethod(); i++ {
+		m := mt.Method(i)
+
+		wm, ok := wmt.MethodByName(m.Name)
+		if !ok {
+			t.Fatalf("Method \"%s\" is present in Machine, but not in WindowsMachine", m.Name)
+		}
+
+		if m.Type.NumIn() != wm.Type.NumIn() {
+			t.Fatalf("Method \"%s\" in Machine has a different number of arguments than WindowsMachine (%d vs %d, respectively)", m.Name, m.Type.NumIn(), wm.Type.NumIn())
+		}
+
+		if m.Type.NumOut() != wm.Type.NumOut() {
+			t.Fatalf("Method \"%s\" in Machine has a different number of outputs than WindowsMachine (%d vs %d, respectively)", m.Name, m.Type.NumOut(), wm.Type.NumOut())
+		}
+
+		// Starting at 1 since the 0th argument is the receiver which is always different.
+		for j := 1; j < m.Type.NumIn(); j++ {
+			if !m.Type.In(j).AssignableTo(wm.Type.In(j)) {
+				t.Fatalf("Argument #%d in method \"%s\" of Machine is different from WindowsMachine",
+					j+1, m.Name)
+			}
+		}
+
+		for j := 0; j < m.Type.NumOut(); j++ {
+			if !m.Type.Out(j).AssignableTo(wm.Type.Out(j)) {
+				t.Fatalf("Output #%d in method \"%s\" of Machine is different from WindowsMachine (%#v vs %#v)",
+					j+1, m.Name, m.Type.Out(j), wm.Type.Out(j))
+			}
+		}
+	}
+}
