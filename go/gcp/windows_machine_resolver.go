e36ce8c764f2223e90af426080040ae5044b89c6
Asanka Herath <asanka@chromium.org>
2018-04-16 12:04:08 -0400

    Add a GCP resolver for asset.windows_machine
    Change-Id: I7792d1038241a3bbd024fc765963b4a7e461b873
    Reviewed-on: https://chromium-review.googlesource.com/1001730
    Reviewed-by: Fei Ling <feiling@chromium.org>

---
+++ go/gcp/windows_machine_resolver.go
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package gcp
+
+import (
+	"chromium.googlesource.com/enterprise/cel/go/asset"
+	"chromium.googlesource.com/enterprise/cel/go/common"
+)
+
+var startup_script_path = common.RefPathMust("host.resources.win_startup")
+var agent_x64_path = common.RefPathMust("host.resources.win_agent_x64")
+
+// WindowsMachine_AddStartupDependencies adds depedencies on the windows
+// startup scripts and windows agent binaries to a machine.
+func windowsMachine_AddStartupDeps(ctx common.Context, m *asset.WindowsMachine) error {
+	err := ctx.PublishDependency(m, startup_script_path)
+	if err != nil {
+		return err
+	}
+
+	return ctx.PublishDependency(m, agent_x64_path)
+}
+
+func init() {
+	common.RegisterResolverFunc(common.AdditionalDependencyResolverKind,
+		windowsMachine_AddStartupDeps)
+}
