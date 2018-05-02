a669a24ada518d40bb10e52614605502c5b8ff5b
Asanka Herath <asanka@chromium.org>
2018-05-02 11:17:14 -0400

    Run Go tests on PRESUBMIT.
    Until such time this project has a CQ, PRESUBMIT is going to take its
    place. All changes after this should build and pass all tests before
    they can be uploaded unless presubmits are skipped.
    
    R=feiling@chromium.org
    
    Change-Id: Ie87c6fce3bdb26740134a852846381f2e2b79067
    Reviewed-on: https://chromium-review.googlesource.com/1033445
    Reviewed-by: Fei Ling <feiling@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -34,9 +34,80 @@ def CheckIfFilesNeedFormatting(input_api, output_api):
   ]
 
 
+def RunGoTests(input_api, output_api):
+  results = []
+  p = input_api.subprocess.Popen(
+      [
+          'go', 'test', '-p={}'.format(input_api.cpu_count), '-json', '-vet',
+          'off', './go/...'
+      ],
+      cwd=input_api.PresubmitLocalPath(),
+      stdout=input_api.subprocess.PIPE,
+      stderr=input_api.subprocess.PIPE)
+  output, err = p.communicate()
+  failures = []
+
+  for o in output.splitlines():
+    d = input_api.json.loads(o)
+    if "Action" not in d or "Test" not in d:
+      continue
+    if d["Action"] != "fail":
+      continue
+    failures.append(d)
+
+  if len(failures) != 0:
+    results.append(
+        output_api.PresubmitError(
+            message="Go tests failed",
+            items=[
+                'Test {} in {}'.format(d["Test"], d["Package"])
+                for d in failures
+            ]))
+
+  return results
+
+
+def RunGoVet(input_api, output_api):
+  results = []
+  p = input_api.subprocess.Popen(
+      ['go', 'vet', './go/...'],
+      cwd=input_api.PresubmitLocalPath(),
+      stdout=input_api.subprocess.PIPE,
+      stderr=input_api.subprocess.PIPE)
+  output, err = p.communicate()
+
+  failures = []
+  for o in err.splitlines():
+    o = o.strip()
+    if o == "" or o.startswith("# "):
+      continue
+    failures.append(o)
+
+  if len(failures) != 0:
+    results.append(
+        output_api.PresubmitError(message='Go vet failed', items=failures))
+  return results
+
+
+def CommonChecks(input_api, output_api):
+  """CommonChecks runs common validation steps that apply to both upload and
+  commit."""
+  results = []
+  results.extend(CheckIfFilesNeedFormatting(input_api, output_api))
+  results.extend(RunGoTests(input_api, output_api))
+  results.extend(RunGoVet(input_api, output_api))
+  results.extend(
+      input_api.canned_checks.PanProjectChecks(input_api, output_api))
+  return results
+
+
 def CheckChangeOnUpload(input_api, output_api):
-  return CheckIfFilesNeedFormatting(input_api, output_api)
+  results = []
+  results.extend(CommonChecks(input_api, output_api))
+  return results
 
 
 def CheckChangeOnCommit(input_api, output_api):
-  return []
+  results = []
+  results.extend(CommonChecks(input_api, output_api))
+  return results
