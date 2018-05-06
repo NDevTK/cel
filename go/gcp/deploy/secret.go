857ab491c15cfb8a5406f71e04482b92315739ff
Asanka Herath <asanka@chromium.org>
2018-05-06 17:09:52 -0400

    Add GCP Secret resolver.
    This resolver handles the work of encrypting a secret and uploading the
    blob of ciphertext to the object store.
    
    Change-Id: I4233f2ff130ed943d70e63cb8e7723ecef25d5d3
    Reviewed-on: https://chromium-review.googlesource.com/1046135
    Reviewed-by: Fei Ling <feiling@chromium.org>

---
+++ go/gcp/deploy/secret.go
@@ -0,0 +1,56 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package deploy
+
+import (
+	"encoding/base64"
+
+	"chromium.googlesource.com/enterprise/cel/go/common"
+	"chromium.googlesource.com/enterprise/cel/go/gcp"
+	cloudkms "google.golang.org/api/cloudkms/v1"
+)
+
+type secret struct{}
+
+func (*secret) ResolveIndexedObjects(ctx common.Context, secret *common.Secret) error {
+	s, err := gcp.SessionFromContext(ctx)
+	if err != nil {
+		return err
+	}
+
+	ks, err := s.GetCloudKmsService()
+	if err != nil {
+		return err
+	}
+
+	keyName := gcp.CryptoKeyResource(s.GetProject(), gcp.KmsLocationId, gcp.KmsKeyRingId,
+		gcp.KmsCryptoKeyId)
+
+	resp, err := ks.Projects.Locations.KeyRings.CryptoKeys.
+		Encrypt(keyName, &cloudkms.EncryptRequest{
+			Plaintext: string(base64.StdEncoding.EncodeToString(secret.Final)),
+		}).
+		Context(ctx).
+		Do()
+	if err != nil {
+		return err
+	}
+
+	ciphertext, err := base64.StdEncoding.DecodeString(resp.Ciphertext)
+	if err != nil {
+		return err
+	}
+
+	ref, err := ctx.GetObjectStore().PutObject(ciphertext)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Publish(secret, "object_reference", ref)
+}
+
+func init() {
+	common.RegisterResolverClass(&secret{})
+}
