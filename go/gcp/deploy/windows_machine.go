43f7f83806730a5e98e036cfed30b9a9042fae7a
Asanka Herath <asanka@chromium.org>
2018-05-10 16:46:58 -0400

    Add full resolver for asset.WindowsMachine.
    This CL completes the set of resolvers needed for deploying
    asset.WindowsMachine resources. The new ConstructedResourceResolver
    emits the necessary items to the GCP deployment manifest.
    
    Change-Id: I0adc0b2ac8a172188154a500dc8b45351b61494f
    Reviewed-on: https://chromium-review.googlesource.com/1054403
    Reviewed-by: Fei Ling <feiling@chromium.org>

--- go/gcp/deploy/windows_machine.go
+++ go/gcp/deploy/windows_machine.go
@@ -7,15 +7,32 @@ package deploy
 import (
 	"chromium.googlesource.com/enterprise/cel/go/asset"
 	"chromium.googlesource.com/enterprise/cel/go/common"
+	"chromium.googlesource.com/enterprise/cel/go/gcp"
+	"chromium.googlesource.com/enterprise/cel/go/gcp/compute"
+	"chromium.googlesource.com/enterprise/cel/go/host"
+	"fmt"
+	google_iam_admin_v1 "google.golang.org/genproto/googleapis/iam/admin/v1"
 )
 
 var startupScriptPath = common.RefPathMust("host.resources.startup.win_startup")
 var agentX64Path = common.RefPathMust("host.resources.startup.win_agent_x64")
+var projectPath = common.RefPathMust("host.project")
+var serviceAccountPath = common.RefPathMust("host.resources.service_account")
+var storageBucketPath = common.RefPathMust("host.storage.bucket")
 
-// WindowsMachine_AddStartupDependencies adds depedencies on the windows
-// startup scripts and windows agent binaries to a machine.
-func windowsMachine_AddStartupDeps(ctx common.Context, m *asset.WindowsMachine) error {
+const windowsStartupScriptMetadataKey = "windows-startup-script-url"
+
+type windowsMachine struct{}
+
+// ResolveAdditionalDependencies adds depedencies on the windows startup
+// scripts and windows agent binaries to a machine.
+func (*windowsMachine) ResolveAdditionalDependencies(ctx common.Context, m *asset.WindowsMachine) error {
 	err := ctx.PublishDependency(m, startupScriptPath)
+	if err != nil {
+		return err
+	}
+
+	err = ctx.PublishDependency(m, projectPath)
 	if err != nil {
 		return err
 	}
@@ -23,7 +40,58 @@ func windowsMachine_AddStartupDeps(ctx common.Context, m *asset.WindowsMachine) 
 	return ctx.PublishDependency(m, agentX64Path)
 }
 
+func (*windowsMachine) ResolveConstructedAssets(ctx common.Context, m *asset.WindowsMachine) error {
+	d := GetDeploymentManifest()
+
+	p := common.Must(ctx.Get(projectPath)).(*host.Project)
+	mt := common.Must(ctx.Indirect(m, "machine_type")).(*host.MachineType)
+	si := common.Must(ctx.Get(serviceAccountPath)).(*google_iam_admin_v1.ServiceAccount)
+
+	var cni []*compute.NetworkInterface
+	for _, ni := range m.NetworkInterface {
+		if ni.FixedAddress != nil {
+			return common.NewNotImplementedError("support for fixed_address in asset.Network")
+		}
+		np := common.Must(ctx.Indirect(ni, "network")).(*asset.Network)
+		cni = append(cni, &compute.NetworkInterface{
+			Network: fmt.Sprintf("$(ref.%s.selfLink)", d.Ref(np)),
+		})
+	}
+
+	ss := gcp.AbsoluteReference(
+		common.Must(ctx.Get(storageBucketPath)).(string),
+		common.Must(ctx.Get(startupScriptPath)).(*common.FileReference).ObjectReference)
+
+	md := &compute.Metadata{
+		Items: []*compute.Metadata_Items{
+			&compute.Metadata_Items{
+				Key:   windowsStartupScriptMetadataKey,
+				Value: ss,
+			},
+		},
+	}
+
+	for _, i := range mt.GetInstanceProperties().Metadata.GetItems() {
+		md.Items = append(md.Items, i)
+	}
+
+	return d.Emit(m, &compute.Instance{
+		Name:              m.Name,
+		Description:       "CEL VM",
+		MachineType:       mt.GetInstanceProperties().MachineType,
+		Zone:              p.Zone,
+		CanIpForward:      mt.GetInstanceProperties().CanIpForward,
+		NetworkInterfaces: cni,
+		Disks:             mt.GetInstanceProperties().Disks,
+		Metadata:          md,
+		ServiceAccounts: []*compute.ServiceAccount{
+			&compute.ServiceAccount{Email: si.Email},
+			// TODO(asanka): Figure out scopes.
+		},
+		Scheduling: mt.GetInstanceProperties().Scheduling,
+	})
+}
+
 func init() {
-	common.RegisterResolverFunc(common.AdditionalDependencyResolverKind,
-		windowsMachine_AddStartupDeps)
+	common.RegisterResolverClass(&windowsMachine{})
 }
