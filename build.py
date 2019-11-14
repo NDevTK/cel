e3dec99ea29780c985bfc930982ebb37b0b5a7d8
Mathieu Binette <mbinette@google.com>
2019-11-14 11:40:43 -0800

    Revert recent build.py change that breaks Mac CI.
    From CL (context: build.py comments & replies):
      https://crrev.com/c/1910970
    
    The build.py change is not necessary after Xiang's
    CL (https://crrev.com/c/1915081) and breaks Mac CI:
      https://ci.chromium.org/swarming/task/48843249bf670c10
    
    Validation:
    - Verified that this fixes Mac CI builds (`zip archive`):
        https://ci.chromium.org/swarming/task/488445ad18236d10
    - Tryjobs below, which include Win10 NestedVM tests:
        https://ci.chromium.org/swarming/task/48844224c5e5b610
    
    Change-Id: I82237b0b917d2f8ee79d2b7f37cd4c39cbfd604f
    Reviewed-on: https://chromium-review.googlesource.com/c/enterprise/cel/+/1917497
    Reviewed-by: Xiang Ji <jxiang@google.com>
    Reviewed-by: Fei Ling <feiling@chromium.org>

--- build.py
+++ build.py
@@ -738,14 +738,7 @@ def _BuildCommand(command,
   if out_dir is None:
     out_dir = _GetBuildDir(build_env)
   _EnsureDir(out_dir)
-
-  # the executables have suffix on non-linux platforms
-  suffix = ''
-  if build_env['GOOS'] == 'windows':
-    suffix = '.exe'
-  if build_env['GOOS'] == 'darwin':
-    suffix = '.darwin'
-
+  suffix = '.exe' if build_env['GOOS'] == 'windows' else ''
   out = os.path.join(out_dir, command + suffix)
   _RunCommand(
       ['go', 'build'] + flags + ['-o', out, package],
